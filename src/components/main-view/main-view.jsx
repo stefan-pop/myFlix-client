0db753f26c0b669831bdf5c0b2144e035fe170bd
Stefan Pop <stefan9161@gmail.com>
2021-06-01 19:16:06 +0200

    created a react component

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class MainView extends React.Component {
+    render() {
+        return (
+            <div className="main-view">
+                <div>Film 1</div>
+                <div>Film 2</div>
+                <div>Film 3</div>
+            </div>
+        );
+    }
+}
