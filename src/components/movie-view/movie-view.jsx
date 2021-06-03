259119ab6c7d341be92aa6252c036d6ef13aff13
Stefan Pop <stefan9161@gmail.com>
2021-06-03 15:00:54 +0200

    extracted the onBackClick prop

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export class MovieView extends React.Component {
 
     render() {
-        const {movie} = this.props;
+        const {movie, onBackClick} = this.props;
         return (
             <div className="movie-view">
                 <div className="movie-image">
