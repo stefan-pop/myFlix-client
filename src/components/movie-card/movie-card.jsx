4b8af68dd4dc035729e85a15b4f02a42a71d5560
Stefan Pop <stefan9161@gmail.com>
2021-06-03 13:01:49 +0200

    extracted the onMovieClick prop and added it to the event listener

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 export class MovieCard extends React.Component {
     render() {
-        const {movieObject} = this.props;
-        return <div className="movie-card">{movieObject.title}</div>;
+        const { movieObject, onMovieClick } = this.props;
+
+        return <div className="movie-card" onClick={() => {onMovieClick(movieObject);}}>{movieObject.title}</div>;
     }
 }
 
