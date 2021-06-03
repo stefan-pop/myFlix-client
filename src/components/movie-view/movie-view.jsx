64d0ad6f785d638f96dbe5a925b5b1b21e2132f8
Stefan Pop <stefan9161@gmail.com>
2021-06-03 19:39:07 +0200

    updated the MovieView component to display more data

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -7,15 +7,23 @@ export class MovieView extends React.Component {
         return (
             <div className="movie-view">
                 <div className="movie-image">
-                    <img src={movie.imagePath} alt="" />
+                    <img src={movie.imagePath} alt="Picture of the movie" />
                 </div>
                 <div className="movie-title">
                     <span className="label">Title: </span>
                     <span className="value">{movie.title}</span>
                 </div>
+                <div className="movie-genre">
+                    <span className="label">Genre: </span>
+                    <span className="value">{movie.genre}</span>
+                </div>
                 <div className="movie-description">
                     <span className="label">Description: </span>
                     <span className="value">{movie.description}</span>
+                </div>
+                <div className="movie-director">
+                    <span className="label">Director: </span>
+                    <span className="value">{movie.director}</span>
                 </div>
                 <button onClick={() => { clickBack(null); }}>Back</button>
             </div>
