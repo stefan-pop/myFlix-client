bd3caf6be3607e4046c918cbdf3c8e8412b23a38
Stefan Pop <stefan9161@gmail.com>
2021-06-03 12:06:04 +0200

    created a MovieView component

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export class MovieView extends React.Component {
+
+    render() {
+        const {movie} = this.props;
+        return (
+            <div className="movie-view">
+                <div className="movie-image">
+                    <img src={movie.imagePath} alt="" />
+                </div>
+                <div className="movie-title">
+                    <span className="label">Title: </span>
+                    <span className="value">{movie.title}</span>
+                </div>
+                <div className="movie-description">
+                    <span className="label">Description: </span>
+                    <span className="value">{movie.description}</span>
+                </div>
+            </div>
+        )
+    }
+}
