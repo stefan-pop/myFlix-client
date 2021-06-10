31857bcd95fe4fe1a9c9094fbc3999e6190a408a
Stefan Pop <stefan9161@gmail.com>
2021-06-10 14:01:15 +0200

    updated the registration form to use a bootstrap form

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 export function RegistrationView(props) {
     const [ username, setUsername ] = useState('');
     const [ pwd, setPwd ] = useState('');
@@ -14,21 +16,33 @@ export function RegistrationView(props) {
     }
 
     return (
-        <form action="">
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" value={ username } onChange={ (e) => setUsername(e.target.value) } />
+        <Form>
+        <h3>Create Account</h3> <hr />
+        <Form.Group controlId="formUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text"   onChange={ (e) => setUsername(e.target.value)} />
+        </Form.Group>
 
-            <label htmlFor="pwd">Password:</label>
-            <input type="password" id="pwd" value={ pwd } onChange={ (e) => setPwd(e.target.value) } />
+        <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password"  onChange={ (e) => setPwd(e.target.value) } />
+        </Form.Group>
 
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" value={ email } onChange={ (e) => setEmail(e.target.value) } />
-
-            <label htmlFor="birth">Birth Year:</label>
-            <input type="text" id="birth" value={ birth_date } onChange={ (e) => setBirthDate(e.target.value) } />
-
-            <button type="button" onClick={handleSubmit}>Create account</button>
-        </form>
+        <Form.Group controlId="formEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email"  onChange={ (e) => setEmail(e.target.value) }/>
+        </Form.Group>
+        
+        <Form.Group controlId="formBirthday">
+            <Form.Label>Birth Date</Form.Label>
+            <Form.Control type="text"  onChange={ (e) => setBirthDate(e.target.value)} />
+        </Form.Group>
+        
+        <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+        <Form.Text className="text-muted">
+            Already have an account? Sign in <a href="#" target="_blank">here</a>
+        </Form.Text>
+    </Form>
     )
 }
 
