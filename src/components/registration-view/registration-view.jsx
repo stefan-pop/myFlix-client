7baa51d489c3c1e6e3150eb2bef6ee67434f38e0
Stefan Pop <stefan9161@gmail.com>
2021-06-15 14:14:18 +0200

    implemented the validation for RegistrationView similar to LoginView

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -18,6 +18,44 @@ export function RegistrationView(props) {
     const [ email, setEmail ] = useState('');
     const [ birth_date, setBirthDate ] = useState('');
 
+    // States for validation
+    const [ validateUser, setValidateUser ] = useState('');
+    const [ validatePassword, setValidatePassword ] = useState('');
+    const [ validateEmail, setValidateEmail ] = useState('');
+
+    
+    // Username validation
+    const validateUsername = (e) => {
+        if (e.target.value.length > 0 && e.target.value.length < 5) {
+            setValidateUser('Username must be longer than 5 characters' );
+        }else {
+            setValidateUser('');
+        }
+
+        if (!e.currentTarget.value.match(/^[0-9a-z]+$/) && e.target.value.length > 0) {
+            setValidateUser('Only alphanumeric characters allowed')
+        }
+    }
+
+    // Password validation
+    const validatePwd = (e) => {
+        if (e.target.value.length > 0 && e.target.value.length < 8) {
+            setValidatePassword('Password must be longer than 8 characters');
+        }else {
+            setValidatePassword('');
+        }
+    }
+
+    // Email validation
+    const validateMail = (e) => {
+        if (!e.target.value.match(/\S+@\S+\.\S+/) && e.target.value.length > 0) {
+            setValidateEmail('Invalid email');
+        }else {
+            setValidateEmail('');
+        }
+    }
+
+
     
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -43,18 +81,18 @@ export function RegistrationView(props) {
         <Form>
             <h3>Create Account</h3> <hr />
             <Form.Group controlId="formUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text"   onChange={ (e) => setUsername(e.target.value)} />
+                <Form.Label>Username <span className="validation-feedback">{validateUser}</span> </Form.Label>
+                <Form.Control type="text"  value={username}  onChange={ (e) => { setUsername(e.target.value), validateUsername(e) }} />
             </Form.Group>
 
             <Form.Group controlId="formPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password"  onChange={ (e) => setPwd(e.target.value) } />
+                <Form.Label>Password  <span className="validation-feedback">{validatePassword}</span> </Form.Label>
+                <Form.Control type="password"  value={pwd} onChange={ (e) => { setPwd(e.target.value), validatePwd(e) }} />
             </Form.Group>
 
             <Form.Group controlId="formEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email"  onChange={ (e) => setEmail(e.target.value) }/>
+                <Form.Label>Email address <span className="validation-feedback">{validateEmail}</span> </Form.Label>
+                <Form.Control type="email" value={email} onChange={ (e) => { setEmail(e.target.value), validateMail(e) }} />
             </Form.Group>
             
             <Form.Group controlId="formBirthday">
