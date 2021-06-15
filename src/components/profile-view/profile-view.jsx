24fabf56a1d4bcd71a0d7954802955745985204e
Stefan Pop <stefan9161@gmail.com>
2021-06-15 13:41:25 +0200

    Updated the ProfileView to greet the user with his actual username

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 
-export function ProfileView({clickBack}) {
+export function ProfileView({clickBack, user}) {
 
     return (
         <div className="profile-view">
-            <h1>Profile, Welcome</h1>
+            <h1>{`Welcome ${user}`}</h1>
             <Button variant="link"  onClick={() => { clickBack(); }}>Back</Button>
         </div>
     )
