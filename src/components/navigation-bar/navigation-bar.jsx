859588a943fd620643798e5efb8ff2f6f56d1e7c
Stefan Pop <stefan9161@gmail.com>
2021-06-15 13:27:54 +0200

    updated the navigation bar so I can nest some Links inside it that will go to different components

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 
+// Router
+import { Link } from "react-router-dom";
+
+// Style
+import './navigation-bar.scss';
+
 // react-bootstrap components
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
 
-export function NavigationBar(props) {
+export function NavigationBar( {logOut} ) {
 
     return(
-        <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark" fixed="top">
-            <Navbar.Brand href="#home">My Flix</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="me-auto">
-                    <NavDropdown title="User" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Favorite Movies</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Sign Out</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.4">Delete Account</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-            </Navbar.Collapse>            
-        </Navbar>
+    <Navbar bg="light" variant="light" fixed="top">
+        <Navbar.Brand href="#">My Flix</Navbar.Brand>
+        <Nav className="me-auto">
+            <Nav.Item className="nav-link">
+                <Link to={`/`}>Home</Link>
+            </Nav.Item>
+        </Nav>
+        <Button size="sm" className="float-end" onClick={logOut}>Log Out</Button>
+    </Navbar>
     )
 }
