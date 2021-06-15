966e08709d27eae644eeb57990a9af1355dfbf29
Stefan Pop <stefan9161@gmail.com>
2021-06-15 14:04:20 +0200

    added a form validation for username and stiled the output warnings

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -5,6 +5,9 @@ import axios from 'axios';
 // Router
 import { Link } from "react-router-dom";
 
+// Style
+import './login-view.scss';
+
 // react-bootstrap components
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -13,7 +16,22 @@ import Navbar from 'react-bootstrap/Navbar';
 export function LoginView(props) {
     const [ username, setUsername ] = useState('');
     const [ password, setPassword ] = useState('');
+    const [ validateUser, setValidateUser ] = useState('');
+    const [ validatePassword, setValidatePassword ] = useState('');
 
+
+    // Username Validation
+    const validateUsername = (e) => {
+        if (e.target.value.length > 0 && e.target.value.length < 5) {
+            setValidateUser('Username must be longer than 5 characters' );
+        }else {
+            setValidateUser('');
+        }
+
+        if (!e.currentTarget.value.match(/^[A-Za-z0-9]+$/) && e.target.value.length > 0) {
+            setValidateUser('Only alphanumeric characters allowed')
+        }
+    }    
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -37,8 +55,8 @@ export function LoginView(props) {
         <Form>
             <h3>Login</h3> <hr />
             <Form.Group controlId="formUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text" onChange={ (e) => setUsername(e.target.value)}  />
+                <Form.Label>Username <span className="validation-feedback">{validateUser}</span></Form.Label>
+                <Form.Control type="text" value={username} onChange={ (e) => {setUsername(e.target.value), validateUsername(e)}} />
             </Form.Group>
 
             <Form.Group controlId="formPassword">
