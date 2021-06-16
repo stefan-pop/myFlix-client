c0d6fab9700e42476b6b69ce2cba98f2c0f58c6d
Stefan Pop <stefan9161@gmail.com>
2021-06-16 16:38:53 +0200

    updated the return of the component to display user details

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -9,7 +9,29 @@ import './profile-view.scss';
 
     return (
         <div className="profile-view">
-            <h1>{`Welcome ${user}`}</h1>
+            <h3>{`Welcome ${userProfile.username}`}</h3> <hr />
+
+            <div className="user-profile">
+                <div className="user-info">
+                    <div className="user-label">Username:</div>
+                    <div className="user">{userProfile.username}</div>
+                </div>
+                <div className="user-info">
+                    <div className="user-label">Email:</div>
+                    <div className="user">{userProfile.email}</div>
+                </div>
+                <div className="user-info">
+                    <div className="user-label">Birth:</div>
+                    <div className="user">{userProfile.birth_date.slice(0, 10)}</div>
+                </div>
+                <div className="user-info">
+                    <div className="user-label">Favorite Movies:</div>
+                    <ul className="user">
+                        {userProfile.favorite_movies.map((id, index) => <li key={index}> <span>{id}</span></li> )}
+                    </ul>
+                </div>
+            </div>
+
             <Button variant="link"  onClick={() => { clickBack(); }}>Back</Button>
         </div>
     )
