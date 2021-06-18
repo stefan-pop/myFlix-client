890356b5a27c5eda2431ff4cc2da28ec058837d5
Stefan Pop <stefan9161@gmail.com>
2021-06-18 13:38:34 +0200

    imported new prop and created the logic to display links to movies belonging a director

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -4,11 +4,18 @@ import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
 
 export function DirectorView({ director, clickBack }) {
+export function DirectorView({ director, clickBack, movies }) {
+
+    const directorsMovies = movies.filter(m => m.director.name === director.name)
 
     return (
         <div className="director-view">
             <h1> {director.name} </h1>
             <h3> {director.bio} </h3>
+            <div className="director-movies">
+                    <small>Movies belonging to this director:</small>
+                    {directorsMovies.map((m, i) => <p key={m._id}> <Link to={`/movies/${m._id}`}>{m.title}</Link> </p> )}
+            </div> <hr />
             <Button variant="link"  onClick={() => { clickBack(); }}>Back</Button>
         </div>
     )
