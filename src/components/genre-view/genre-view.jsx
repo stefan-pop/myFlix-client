6566245a3772e0a8338fa6effc6d9868851fb620
Stefan Pop <stefan9161@gmail.com>
2021-06-18 13:42:26 +0200

    implemented the logic to display movies belonging to a specific genre.
    identic to DirectorView

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -3,14 +3,26 @@ import React from 'react';
 import Button from 'react-bootstrap/Button'
 import PropTypes from 'prop-types';
 
+// Router
+import { Link } from "react-router-dom";
+
 export function GenreView(props) {
 
-    const { genre, clickBack } = props;
+    const { genre, clickBack, movies } = props;
+
+    const genresMovies = movies.filter(m => m.genre.name === genre.name)
+
 
     return (
         <div className="director-view">
-            <h1> {genre.name} </h1>
-            <h3> {genre.description} </h3>
+            <h2> {genre.name} </h2>
+            <p> {genre.description} </p>
+
+            
+            <div className="genre-movies">
+                <small>Movies belonging to this genre:</small>
+                {genresMovies.map((m, i) => <p key={m._id}> <Link to={`/movies/${m._id}`}>{m.title}</Link> </p> )}
+            </div> <hr />
             <Button variant="link"  onClick={() => { clickBack(); }}>Back</Button>
         </div>
     )
@@ -22,5 +34,6 @@ GenreView.propTypes = {
         name: PropTypes.string.isRequired,
         description: PropTypes.string
     }).isRequired,
-    clickBack: PropTypes.func.isRequired
+    clickBack: PropTypes.func.isRequired,
+    movies: PropTypes.array.isRequired
 }
